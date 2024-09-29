af78b010917aa331ed0045262a4563e28847e87c
Patr1arch <43929725+Patr1arch@users.noreply.github.com>
2024-09-30 01:24:21 +1000

    Add Hello Cotroller and Service

---
+++ src/Service/HelloService.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Service;
+
+class HelloService
+{
+    private const MIN_LUCKY_NUMBER = 1;
+    private const MAX_LUCKY_NUMBER = 3;
+
+    public function generateLuckyNumber(): string
+    {
+        return rand(self::MIN_LUCKY_NUMBER, self::MAX_LUCKY_NUMBER);
+    }
+}
